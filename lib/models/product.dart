45cc2825552ea8a4a0ead50102c011e4a874cea1
Faris Al <faris.al.ft17@mail.umy.ac.id>
2022-08-22 20:41:48 +0700

    Home Page & get API

---
+++ lib/models/product.dart
@@ -0,0 +1,90 @@
+import 'dart:convert';
+
+All allFromJson(String str) => All.fromJson(json.decode(str));
+
+String allToJson(All data) => json.encode(data.toJson());
+
+class All {
+  All({
+    required this.products,
+    this.total,
+    this.skip,
+    this.limit,
+  });
+
+  List<Product> products;
+  int? total;
+  int? skip;
+  int? limit;
+
+  factory All.fromJson(Map<String, dynamic> json) => All(
+        products: List<Product>.from(
+            json["products"].map((x) => Product.fromJson(x))),
+        total: json["total"],
+        skip: json["skip"],
+        limit: json["limit"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "products": List<dynamic>.from(products.map((x) => x.toJson())),
+        "total": total,
+        "skip": skip,
+        "limit": limit,
+      };
+}
+
+class Product {
+  Product({
+    required this.id,
+    required this.title,
+    required this.description,
+    required this.price,
+    this.discountPercentage,
+    this.rating,
+    required this.stock,
+    required this.brand,
+    required this.category,
+    required this.thumbnail,
+    required this.images,
+  });
+
+  int id;
+  String title;
+  String description;
+  int price;
+  double? discountPercentage;
+  double? rating;
+  int stock;
+  String brand;
+  String category;
+  String thumbnail;
+  List<String> images;
+
+  factory Product.fromJson(Map<String, dynamic> json) => Product(
+        id: json["id"],
+        title: json["title"],
+        description: json["description"],
+        price: json["price"],
+        discountPercentage: json["discountPercentage"].toDouble(),
+        rating: json["rating"].toDouble(),
+        stock: json["stock"],
+        brand: json["brand"],
+        category: json["category"],
+        thumbnail: json["thumbnail"],
+        images: List<String>.from(json["images"].map((x) => x)),
+      );
+
+  Map<String, dynamic> toJson() => {
+        "id": id,
+        "title": title,
+        "description": description,
+        "price": price,
+        "discountPercentage": discountPercentage,
+        "rating": rating,
+        "stock": stock,
+        "brand": brand,
+        "category": category,
+        "thumbnail": thumbnail,
+        "images": List<dynamic>.from(images.map((x) => x)),
+      };
+}
--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:sell_product/models/product.dart';
+import 'package:sell_product/services/remote_service.dart';
+import 'package:flutter/widgets.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -8,41 +11,76 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  List<Product>? products;
+
+  bool isLoaded = false;
+
+  @override
+  void initState() {
+    super.initState();
+    getData();
+  }
+
+  getData() async {
+    products = await RemoteService().getProducts();
+    if (products != null) {
+      setState(() {
+        isLoaded = true;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Sell Product App"),
       ),
-      body: GridView.builder(
-          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2),
-          itemBuilder: (context, index) {
-            return Container(
-              margin: const EdgeInsets.all(2),
-              child: Card(
-                  color: Colors.white,
-                  elevation: 3,
-                  child: TextButton(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        const Icon(
-                          Icons.abc,
-                          color: Colors.deepOrange,
-                          size: 100,
-                        ),
-                        const Text(
-                          "Hello",
-                          style: TextStyle(fontSize: 20),
-                          overflow: TextOverflow.ellipsis,
-                        )
-                      ],
-                    ),
-                    onPressed: () {},
-                  )),
-            );
-          }),
+      body: Visibility(
+          replacement: const Center(
+            child: CircularProgressIndicator(),
+          ),
+          visible: isLoaded,
+          child: GridView.builder(
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                childAspectRatio: MediaQuery.of(context).size.width /
+                    (MediaQuery.of(context).size.height / 1.5)),
+            itemCount: products?.length,
+            itemBuilder: (context, index) {
+              return Container(
+                margin: const EdgeInsets.all(2),
+                child: Card(
+                    color: Colors.white,
+                    elevation: 3,
+                    child: TextButton(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          Image.network(
+                            products![index].thumbnail,
+                            scale: 0.1,
+                          ),
+                          Text(
+                            products![index].title,
+                            style: const TextStyle(
+                                fontSize: 14, color: Colors.black),
+                            overflow: TextOverflow.clip,
+                            textAlign: TextAlign.left,
+                          ),
+                          Text(
+                            "\$${products![index].price}",
+                            style: const TextStyle(fontSize: 14),
+                            overflow: TextOverflow.clip,
+                            textAlign: TextAlign.left,
+                          ),
+                        ],
+                      ),
+                      onPressed: () {},
+                    )),
+              );
+            },
+          )),
     );
   }
 }
